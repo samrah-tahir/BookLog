4bfdd04e100dd3d9b91ac966bc2aacc0f93b79fd
samrah-tahir <samrah.tahir11@gmail.com>
2022-08-03 20:02:49 +0400

    Refactored library module

--- script.js
+++ script.js
@@ -1,7 +1,127 @@
 
-(function(){
-    const bookStorage = [];
+/** Note: library must have these funcs:
+ * add book (O)
+ * delete book
+ * search book
+ * Book should have:
+ * get read status
+ * create book by returning book object
+ */
+
+/**PURPOSE: create books
+ * 
+ */
+function Book(image, title, author, pages, status){
+
+    return {
+        image: image,
+        title: title,
+        author: author,
+        noOfPages: pages,
+        status: status,
+    }
+}
+
+/**PURPOSE: manipulate UI
+ * 
+ */
+function UI(){
+    const displayBooks = function(myLibrary){
+        const bookBox = document.querySelector('#book-container');
+        
+        if(myLibrary){
+            bookBox.innerText = '';
+            myLibrary.forEach((book, index) => {
+                bookBox.appendChild(createBookCard(book, index));
+            });
+        }
+        
+    }
+
+    const createBookCard = function(book, index){
+        const newBook = document.createElement('div');
+        const bookDetails = document.createElement('div');
+        const bookImg = document.createElement('div');
+        const bookTitle = document.createElement('h5');
+        const bookAuthor = document.createElement('p');
+        const readStatusIndicator = document.createElement('div');
+        const bookThumbnail = book.image;
+        const bookDeleteBtn = document.createElement('button');
+        bookDeleteBtn.type = 'button';
+        bookDeleteBtn.innerText = 'Delete';
+
+        bookDeleteBtn.classList.add('delete-btn');
+        newBook.classList.add('book');
+        bookDetails.classList.add('book-details');
+        bookImg.classList.add('book-img');
+
+        newBook.dataset.id = index; //Adding id to each book for easy access
+        // book.id = newBook.dataset.id;
+
+        if(book.status == true){
+            readStatusIndicator.classList.add('read-status');
+            readStatusIndicator.innerText = 'Completed';
+        }
+        else {
+            readStatusIndicator.classList.add('in-progress');
+            readStatusIndicator.innerText = 'In Progress';
+        }
+
+        bookTitle.innerText = `${book.title}`;
+        bookAuthor.innerText = `${book.author}`;
+        bookImg.style.backgroundImage = `url(${bookThumbnail})`;
+        
+        newBook.appendChild(bookImg);
+        bookImg.appendChild(bookDeleteBtn);
+        newBook.appendChild(bookDetails);
+        newBook.appendChild(readStatusIndicator);
+        bookDetails.appendChild(bookTitle);
+        bookDetails.appendChild(bookAuthor);
+
+        return newBook;
+    }   
+
+    const createSearchRes = function(noRes){
+        let search_result = document.createElement('div');
+        search_result.classList.add('search_result');
     
+        if(noRes.hasOwnProperty('imageLinks') && noRes.authors && noRes.title){
+        
+            let resultIimgDiv = document.createElement('div');
+            resultIimgDiv.classList.add('result-img');
+
+            let resultImg = document.createElement('img')
+            resultImg.src = noRes.imageLinks.thumbnail;
+
+            let resultBookDetails = document.createElement('div');
+            resultBookDetails.classList.add('result-book-details');
+
+            let bookName = document.createElement('h5');
+            bookName.innerText = noRes.title;
+
+            let authorName = document.createElement('p');
+            authorName.innerText = noRes.authors[0];
+            resultBookDetails.appendChild(bookName);
+            resultBookDetails.appendChild(authorName);
+
+            search_result.appendChild(resultIimgDiv);
+            search_result.appendChild(resultBookDetails);
+
+            resultIimgDiv.appendChild(resultImg);
+
+            document.querySelector('.search-results').appendChild(search_result);
+        }
+
+    }
+    return {displayBooks, 
+            createBookCard, 
+            createSearchRes,
+        };
+}
+
+const library = (function(){
+    const bookStorage = [];
+    const bookUI = UI();
     //cached DOM
     const bookContainer = document.querySelector('#book-container');
     const modal = document.querySelector('.book-modal');
@@ -24,21 +144,54 @@
     //bind events
 
 
+    function addBookToDashboard(book){
+        bookStorage.push(book);
+       _resetForm();
+        bookUI.displayBooks(bookStorage);
+    }
+
+    function deleteBook(e){
+        e.stopPropagation();
+ 
+        let book = e.target.parentElement.parentElement;
+        bookStorage.splice(book.dataset.id, 1);
+        bookUI.displayBooks(bookStorage);
+    }
+
+    //to get book from API
+    function searchBookOnline(bookName, bookAuthor){
+        YOUR_KEY_HERE = ''
+        fetch('https://www.googleapis.com/books/v1/volumes?q='+bookName+'+'
+        +'inauthor:'+bookAuthor+'&fields=kind,items(volumeInfo(authors,title,imageLinks))'
+        +YOUR_KEY_HERE, {mode: 'cors'})
+            .then(function(response) {
+                 if(response.ok)
+                    return response.json();
+            })
+            .then(function(response) {
+                let searchResults = response.items;
+                searchResults.forEach((res) => bookUI.createSearchRes(res.volumeInfo));
+                //createSearchRes(response.items[0].volumeInfo);
+               // document.querySelector('.result-img > img').src = response.items[0].volumeInfo.imageLinks.thumbnail;
+            })
+            .catch((err) => {
+                console.log('Error: ', err);
+            });
+        // createSearchRes('yuh');
+    }
+
     searchResContainer.addEventListener('click', (e) => {
         if(e.target.classList.contains('search_result')){
-            console.log('hi')
-            bookTitle.value = e.target.lastChild.firstChild.innerText;
-            bookAuthor.value = e.target.lastChild.lastChild.innerText;
-            uploaded_image = e.target.firstChild.firstChild.src;
-            pages.value = 0;
-            readStatus.checked = false;
-            addBookToDashboard();
+            let book = getBookInfo(e.target.firstChild.firstChild.src, 
+                e.target.lastChild.lastChild.innerText,
+                e.target.lastChild.lastChild.innerText, 0, false);
+
+            addBookToDashboard(book);
             modal.classList.remove('active')
         }
         
         
     });
-
 
     //addSelectedBookBtn.addEventListener('click', addBookToDashboard);
     
@@ -96,31 +249,15 @@
         }
     });
 
-    submitBookBtn.addEventListener('click', addBookToDashboard);
+    submitBookBtn.addEventListener('click', () => {
+        let book = getBookInfo(uploaded_image, bookTitle.value, 
+            bookAuthor.value, pages.value, readStatus.checked);
+        addBookToDashboard(book);
+    });
 
-    function addBookToDashboard(){
-        let newBook = {
-            image: uploaded_image,
-            bookName: bookTitle.value,
-            bookAuthor: bookAuthor.value,
-            noOfPages: pages.value,
-            read: readStatus.checked,
-        }
-
-        bookStorage.push(newBook);
-
-       _resetForm();
-
-      
-        displayBooks(bookStorage);
-    }
-
-    function deleteBook(e){
-       e.stopPropagation();
-
-       let book = e.target.parentElement.parentElement;
-       bookStorage.splice(book.dataset.id, 1);
-       displayBooks(bookStorage);
+    function getBookInfo(image, title, author, pages, status){
+        let newBook = Book(image, title, author, pages, status);
+        return newBook;
     }
 
     function _resetForm(){
@@ -131,113 +268,7 @@
         pages.value = '';
     }
 
-    function displayBooks(myLibrary){
-        const bookBox = document.querySelector('#book-container');
-        
-        if(bookStorage){
-            bookBox.innerText = '';
-            myLibrary.forEach((book, index) => {
-                bookBox.appendChild(createBookCard(book, index));
-            });
-        }
-        
-    }
     
-    function createBookCard(book, index){
-        const newBook = document.createElement('div');
-        const bookDetails = document.createElement('div');
-        const bookImg = document.createElement('div');
-        const bookTitle = document.createElement('h5');
-        const bookAuthor = document.createElement('p');
-        const readStatusIndicator = document.createElement('div');
-        const bookThumbnail = book.image;
-        const bookDeleteBtn = document.createElement('button');
-        bookDeleteBtn.type = 'button';
-        bookDeleteBtn.innerText = 'Delete';
-
-        bookDeleteBtn.classList.add('delete-btn');
-        newBook.classList.add('book');
-        bookDetails.classList.add('book-details');
-        bookImg.classList.add('book-img');
-
-        newBook.dataset.id = index; //Adding id to each book for easy access
-        // book.id = newBook.dataset.id;
-
-        if(book.read == true){
-            readStatusIndicator.classList.add('read-status');
-            readStatusIndicator.innerText = 'Completed';
-        }
-        else {
-            readStatusIndicator.classList.add('in-progress');
-            readStatusIndicator.innerText = 'In Progress';
-        }
-
-        bookTitle.innerText = `${book.bookName}`;
-        bookAuthor.innerText = `${book.bookAuthor}`;
-        bookImg.style.backgroundImage = `url(${bookThumbnail})`;
-        
-        newBook.appendChild(bookImg);
-        bookImg.appendChild(bookDeleteBtn);
-        newBook.appendChild(bookDetails);
-        newBook.appendChild(readStatusIndicator);
-        bookDetails.appendChild(bookTitle);
-        bookDetails.appendChild(bookAuthor);
-
-        return newBook;
-    }   
-
-    //to get book from API
-    function searchBookOnline(bookName, bookAuthor){
-        YOUR_KEY_HERE = ''
-        fetch('https://www.googleapis.com/books/v1/volumes?q='+bookName+'+'+'inauthor:'+bookAuthor+'&fields=kind,items(volumeInfo(authors,title,imageLinks))'+YOUR_KEY_HERE, {mode: 'cors'})
-            .then(function(response) {
-                 if(response.ok)
-                    return response.json();
-            })
-            .then(function(response) {
-                let searchResults = response.items;
-                searchResults.forEach((res) => createSearchRes(res.volumeInfo));
-                //createSearchRes(response.items[0].volumeInfo);
-               // document.querySelector('.result-img > img').src = response.items[0].volumeInfo.imageLinks.thumbnail;
-            })
-            .catch((err) => {
-                console.log('Error: ', err);
-            });
-        // createSearchRes('yuh');
-    }
-
-    function createSearchRes(noRes){
-        let search_result = document.createElement('div');
-        search_result.classList.add('search_result');
-    
-        if(noRes.hasOwnProperty('imageLinks') && noRes.authors && noRes.title){
-        
-            let resultIimgDiv = document.createElement('div');
-            resultIimgDiv.classList.add('result-img');
-
-            let resultImg = document.createElement('img')
-            resultImg.src = noRes.imageLinks.thumbnail;
-
-            let resultBookDetails = document.createElement('div');
-            resultBookDetails.classList.add('result-book-details');
-
-            let bookName = document.createElement('h5');
-            bookName.innerText = noRes.title;
-
-            let authorName = document.createElement('p');
-            authorName.innerText = noRes.authors[0];
-            resultBookDetails.appendChild(bookName);
-            resultBookDetails.appendChild(authorName);
-
-            search_result.appendChild(resultIimgDiv);
-            search_result.appendChild(resultBookDetails);
-
-            resultIimgDiv.appendChild(resultImg);
-
-            document.querySelector('.search-results').appendChild(search_result);
-        }
-
-    }
 })();
 
 
